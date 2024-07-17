867d42c833b9b934fa8bf0155d895c989651275d
shuji <shumiyu@example.com>
2024-07-17 15:39:36 +0900

    不要なファイル削除

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (App::environment(['production'])) {
             URL::forceScheme('https');
+            
         }
 
          Paginator::useBootstrap();
